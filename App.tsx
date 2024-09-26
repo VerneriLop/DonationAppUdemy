9761388990095e4a4553c13bc3bdcc2b6af936d2
Verneri <verneri.lopperi@gmail.com>
2024-09-26 13:36:57 +0300

    navigation and styling settings for home page

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import MainNavigation from './navigation/MainNavigation';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = (): React.JSX.Element => {
-  return <SafeAreaView />;
+  return (
+    <NavigationContainer>
+      <MainNavigation />
+    </NavigationContainer>
+  );
 };
 
 export default App;
---
+++ screens/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+import globalStyle from '../../assets/styles/globalStyle';
+
+export const Home = () => {
+  return (
+    <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
+      <View>
+        <Text>Hello world!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
