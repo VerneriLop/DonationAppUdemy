40f5ea8a32fe04b9266dc39411ad327c504575e2
Verneri <verneri.lopperi@gmail.com>
2024-09-27 14:43:58 +0300

    button component ready

--- screens/Home/Home.tsx
+++ screens/Home/Home.tsx
@@ -4,13 +4,19 @@ import {SafeAreaView, Text, View} from 'react-native';
 import Header from '../../components/Header/Header';
 
 import globalStyle from '../../assets/styles/globalStyle';
+import Button from '../../components/Button/Button';
 
 export const Home = (): JSX.Element => {
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
       <Header title="Azzari A." type={1} />
-      <Header title="Azzari A." type={2} />
-      <Header title="Azzari A." type={3} />
+      <Button
+        title="Donate"
+        onPress={() => {
+          console.log('You just pressed me!');
+        }}
+      />
+      <Button title="Donate" isDisabled />
     </SafeAreaView>
   );
 };
