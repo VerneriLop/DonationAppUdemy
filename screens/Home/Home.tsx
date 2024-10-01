86985800e9b691df77646548cc1faa4b662fe3bf
Verneri <verneri.lopperi@gmail.com>
2024-10-01 19:08:28 +0300

    usedispatch testing

--- screens/Home/Home.tsx
+++ screens/Home/Home.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
-import {useSelector} from 'react-redux';
+import {Pressable, SafeAreaView, Text} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
 
 import Header from '../../components/Header/Header';
 import globalStyle from '../../assets/styles/globalStyle';
 import {RootState} from '../../redux/store';
+import {updateFirstName} from '../../redux/reducers/User';
 
 export const Home = (): JSX.Element => {
   const user = useSelector((state: RootState) => state.user);
   console.log(user);
+  const dispatch = useDispatch();
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
       <Header title={user.firstName + ' ' + user.lastName} />
+      <Pressable onPress={() => dispatch(updateFirstName({firstName: 'V'}))}>
+        <Text>Press me to change first name</Text>
+      </Pressable>
     </SafeAreaView>
   );
 };
