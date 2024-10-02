d2102083235609195a94b7066575b0fd2923390e
Verneri <verneri.lopperi@gmail.com>
2024-10-02 13:52:54 +0300

    persist store

--- redux/store.ts
+++ redux/store.ts
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux';
 import {configureStore} from '@reduxjs/toolkit';
-//import {logger} from 'redux-logger';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {persistReducer, persistStore} from 'redux-persist';
+import {logger} from 'redux-logger';
 import User from './reducers/User';
 
 export type RootState = ReturnType<typeof rootReducer>;
@@ -9,11 +11,22 @@ const rootReducer = combineReducers({
   user: User,
 });
 
+const configuration = {
+  key: 'root',
+  storage: AsyncStorage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(configuration, rootReducer);
+
 const store = configureStore({
-  reducer: rootReducer,
-  /*middleware: getDefaultMiddleware => {
-    return getDefaultMiddleware().concat(logger);
-  },*/
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware => {
+    return getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(logger);
+  },
 });
 
 export default store;
+export const persistor = persistStore(store);
