a2f384624943f15279f7d8e526e901f8ef2ace0a
Verneri <verneri.lopperi@gmail.com>
2024-10-09 20:37:34 +0300

    backbutton and navigating back from singleitemscreen

--- screens/SingleDonationItem/SingleDonationItemScreen.tsx
+++ screens/SingleDonationItem/SingleDonationItemScreen.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
-
-import style from './style';
 import {useSelector} from 'react-redux';
 import {RootState} from '../../redux/store';
+import {SafeAreaView, ScrollView} from 'react-native';
 
-const SingleDonationItemScreen = () => {
+import BackButton from '../../components/BackButton/BackButton';
+
+import style from './style';
+import globalStyle from '../../assets/styles/globalStyle';
+
+const SingleDonationItemScreen = ({navigation}: any): JSX.Element => {
   const donationItemInformation = useSelector(
     (state: RootState) => state.donations.selectedDonationInformation,
   );
-  console.log(donationItemInformation);
-  return null;
+
+  return (
+    <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
+      <ScrollView showsVerticalScrollIndicator={false} style={style.container}>
+        <BackButton onPress={() => navigation.goBack()} />
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
 export default SingleDonationItemScreen;
