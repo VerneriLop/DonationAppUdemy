4d8a0d1768228c6d857e36ffb6be0d67df52c0c9
Verneri <verneri.lopperi@gmail.com>
2024-10-10 11:40:19 +0300

    Login screen layout ready

--- screens/Login/Login.tsx
+++ screens/Login/Login.tsx
@@ -1,21 +1,44 @@
 import React, {useState} from 'react';
-import {SafeAreaView, ScrollView} from 'react-native';
+import {Pressable, SafeAreaView, ScrollView, View} from 'react-native';
 
 import style from './style';
 import globalStyle from '../../assets/styles/globalStyle';
 import Input from '../../components/Input/Input';
+import Header from '../../components/Header/Header';
+import Button from '../../components/Button/Button';
 
 const Login = (): JSX.Element => {
   const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
       <ScrollView contentContainerStyle={style.container}>
-        <Input
-          keyboardType={'email-address'}
-          label={'Email'}
-          placeholder={'Enter your email...'}
-          onChangeText={value => setEmail(value)}
-        />
+        <View style={globalStyle.marginBottom24}>
+          <Header type={1} title={'Welcome back'} />
+        </View>
+        <View style={globalStyle.marginBottom24}>
+          <Input
+            keyboardType={'email-address'}
+            label={'Email'}
+            placeholder={'Enter your email...'}
+            onChangeText={value => setEmail(value)}
+          />
+        </View>
+        <View style={globalStyle.marginBottom24}>
+          <Input
+            secureTextEntry
+            label={'Password'}
+            placeholder={'******'}
+            onChangeText={value => setPassword(value)}
+          />
+        </View>
+        <View style={globalStyle.marginBottom24}>
+          <Button title={'Login'} />
+        </View>
+        <Pressable style={style.registrationButton}>
+          <Header color={'#156CF7'} type={3} title={"Don't have an account?"} />
+        </Pressable>
       </ScrollView>
     </SafeAreaView>
   );
