edd64033d356eef03fbc34da5b06dba29b59967c
Verneri <verneri.lopperi@gmail.com>
2024-10-16 16:57:46 +0300

    types for server

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,14 +1,13 @@
-const express = require('express');
+import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.get('/',(req,res)=> {
+app.get('/', (req: Request, res: Response) => {
     res.send('Hello world!');
 });
 
 const PORT = 3000;
 
-app.listen(PORT, () =>{
-    console.log("Server is runnin on port", PORT);
+app.listen(PORT, () => {
+    console.log("Server is running on port", PORT);
 });
-
