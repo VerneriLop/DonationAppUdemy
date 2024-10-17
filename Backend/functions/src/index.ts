d2e36bb73ffdb1eed53d839edbc298b7861f54c6
Verneri <verneri.lopperi@gmail.com>
2024-10-17 15:29:25 +0300

    transferred local setup to firebase and server is running there

--- Backend/functions/src/index.ts
+++ Backend/functions/src/index.ts
@@ -8,7 +8,40 @@
  */
 
 import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+//import * as logger from "firebase-functions/logger";
+
+//import * as functions from 'firebase-functions';
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello world!');
+});
+
+app.get('/user', (req: Request, res: Response) => {
+    res.send('You are getting the user data back!');
+});
+
+app.post('/user', (req: Request, res: Response) => {
+    const { firstName } = req.body;
+    console.log(req.body);
+    res.send(`We created a user with firstname of ${firstName}`);
+});
+
+app.delete('/user', (req: Request, res: Response) => {
+    const { firstName } = req.body;
+    console.log(req.body);
+    res.send(`We deleted a user with firstname of ${firstName}`);
+});
+
+app.put('/user', (req: Request, res: Response) => {
+    const { firstName } = req.body;
+    console.log(req.body);
+    res.send(`We updated a user with firstname of ${firstName}`);
+});
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -17,3 +50,5 @@ import * as logger from "firebase-functions/logger";
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+export const helloWorld = onRequest(app);
