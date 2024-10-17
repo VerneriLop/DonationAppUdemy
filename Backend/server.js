0d431bdabf8d389bbf2dd66bea61341760b92310
Verneri <verneri.lopperi@gmail.com>
2024-10-17 10:19:29 +0300

    bodyparser and testing post,put and delete methods with postman

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,30 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello world!');
+});
+
+app.get('/user', (req, res) => {
+    res.send('You are getting the user data back!');
+});
+
+app.post('/user',(req,res)=>{
+    console.log(req.body);
+    res.send("We created a user with firstname of "+req.body.firstName);
+});
+
+app.delete('/user',(req,res)=>{
+    console.log(req.body);
+    res.send("We deleted a user with firstname of "+req.body.firstName);
+});
+
+app.put('/user',(req,res)=>{
+    console.log(req.body);
+    res.send("We updated a user with firstname of "+req.body.firstName);
 });
 
 const PORT = 3000;
