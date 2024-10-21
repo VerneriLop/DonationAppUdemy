fa7fe1216a43076aea1b5b592e775c1cbd3dd3f8
Verneri <verneri.lopperi@gmail.com>
2024-10-21 12:40:19 +0300

    prettier added for previous files

--- Backend/server.js
+++ Backend/server.js
@@ -5,30 +5,30 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+  res.send('Hello world!');
 });
 
 app.get('/user', (req, res) => {
-    res.send('You are getting the user data back!');
+  res.send('You are getting the user data back!');
 });
 
-app.post('/user',(req,res)=>{
-    console.log(req.body);
-    res.send("We created a user with firstname of "+req.body.firstName);
+app.post('/user', (req, res) => {
+  console.log(req.body);
+  res.send('We created a user with firstname of ' + req.body.firstName);
 });
 
-app.delete('/user',(req,res)=>{
-    console.log(req.body);
-    res.send("We deleted a user with firstname of "+req.body.firstName);
+app.delete('/user', (req, res) => {
+  console.log(req.body);
+  res.send('We deleted a user with firstname of ' + req.body.firstName);
 });
 
-app.put('/user',(req,res)=>{
-    console.log(req.body);
-    res.send("We updated a user with firstname of "+req.body.firstName);
+app.put('/user', (req, res) => {
+  console.log(req.body);
+  res.send('We updated a user with firstname of ' + req.body.firstName);
 });
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log("Server is running on port", PORT);
+  console.log('Server is running on port', PORT);
 });
