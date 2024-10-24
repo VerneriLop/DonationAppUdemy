80a1bda17f5b49a90417ff464a03a1bc0eeeb19c
Verneri <verneri.lopperi@gmail.com>
2024-10-24 09:43:18 +0300

    setting up payment screen

--- Frontend/screens/Payment/Payment.tsx
+++ Frontend/screens/Payment/Payment.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 
-import {SafeAreaView, ScrollView, View} from 'react-native';
+import {SafeAreaView, ScrollView, Text, View} from 'react-native';
 import globalStyle from '../../assets/styles/globalStyle';
+import Header from '../../components/Header/Header';
+import style from './style';
+import {RootState} from '../../redux/store';
+import {useSelector} from 'react-redux';
+import Button from '../../components/Button/Button';
 
 const Payment = () => {
+  const donationItemInformation = useSelector(
+    (state: RootState) => state.donations.selectedDonationInformation,
+  );
+
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
-      <ScrollView>
+      <ScrollView contentContainerStyle={style.paymentContainer}>
+        <Header title={'Making donation'} />
+        <Text style={style.donationAmountDescription}>
+          You are about to donate {donationItemInformation?.price}
+        </Text>
         <View></View>
       </ScrollView>
+      <View style={style.button}>
+        <Button title={'Donate'} />
+      </View>
     </SafeAreaView>
   );
 };
